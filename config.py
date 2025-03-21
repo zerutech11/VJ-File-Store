88ebbb58dd2fef00cf2c250132d88ce33586dc8a
Zeru Tech <zerubabelbirhanu4528@gmail.com>
2025-03-21 17:35:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -18,24 +18,24 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "20658776"))
+API_HASH = environ.get("API_HASH", "7315ea10cb826c6a3d809d5723657a97")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7559990071:AAEiB6Y7zIU589D-fGLQ3CXrM8kTFg-eqN0")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1965659175').split()]
+BOT_USERNAME = environ.get("BOT_USERNAME", "weirdspacejarbot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "client = pymongo.MongoClient("mongodb+srv://zerubabelbirhanu0944:<db_password>@cluster0.4rgbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "")
+DB_URI = environ.get("DB_URI", "client = pymongo.MongoClient("mongodb+srv://zerubabelbirhanu0944:<db_password>@cluster0.4rgbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002629401745"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
